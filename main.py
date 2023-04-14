fe994ba5abb33314e06c77254e54564b524ec245
JCLindo10 <JCLindo10@gmail.com>
2023-04-13 22:45:02 -0400

    general logic flow

--- main.py
+++ main.py
@@ -8,6 +8,34 @@
 
 """
 
+import sys
+
+
+def main():
+    """
+    Main entry point for the caeser-cipher program.
+
+    """
+
+    # option e = encrypt
+    # using option e, the user must provide a shift value and a file to encrypt
+    if sys.argv[1] == "e":
+        shift = int(sys.argv[2])
+        file = sys.argv[3]
+        encrypt(shift, file)
+
+    # option d = decrypt
+    # using option d, the user must provide a file to decrypt. The user also has the option to provide a shift value.
+    if sys.argv[1] == "d":
+        if len(sys.argv) == 3:
+            file = sys.argv[2]
+            decrypt(file)
+        else:
+            shift = int(sys.argv[2])
+            file = sys.argv[3]
+            decrypt(file, shift)
+
+
 if __name__ == "__main__":
 
-    print("Hello World!")
+    main()
