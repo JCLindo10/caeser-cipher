d12ad5bf77bc0ee1955e2c5cd4b154a7873ee199
JCLindo10 <JCLindo10@gmail.com>
2023-04-17 11:34:29 -0400

    more tests

--- test/test.py
+++ test/test.py
@@ -12,6 +12,20 @@ sys.path.append("..")
 
 
 class TestCaeserCipher(unittest.TestCase):
-    def test_encrypt(self):
+    def test_encrypt_equal(self):
         self.assertEqual(main.encrypt("e", 4, "test.txt"), "alex")
         self.assertEqual(main.encrypt("e", 30, "test.txt"), "alex")
+
+    def test_encrypt_not_equal(self):
+        self.assertNotEqual(main.encrypt("e", 7, "test.txt"), "alex")
+        self.assertNotEqual(main.encrypt("e", 3, "test.txt"), "alex")
+
+    def test_decrypt_equal(self):
+        self.assertEqual(main.decrypt("test.txt", 4), "what")
+        self.assertEqual(main.decrypt("test.txt", 30), "what")
+        self.assertEqual(main.decrypt("test.txt"), "what")
+
+    def test_decrypt_not_equal(self):
+        self.assertNotEqual(main.decrypt("test.txt", 7), "what")
+        self.assertNotEqual(main.decrypt("test.txt", 3), "what")
+        self.assertNotEqual(main.decrypt("test.txt"), "tahw")
