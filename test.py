4a764d3792381e76a054ee15bf43c0cc1ac5d1c4
JCLindo10 <JCLindo10@gmail.com>
2023-04-17 11:59:37 -0400

    tests now work

--- test.py
+++ test.py
@@ -5,30 +5,30 @@
     Created: 4/13/2023
 
 """
-from src import main
+import cipher as Cipher
 import unittest
-import sys
-sys.path.append("..")
+# import sys
+# sys.path.append("..")
 
 
 class TestCaeserCipher(unittest.TestCase):
     def test_encrypt_equal(self):
-        self.assertEqual(main.encrypt("e", 4, "test.txt"), "alex")
-        self.assertEqual(main.encrypt("e", 30, "test.txt"), "alex")
+        self.assertEqual(Cipher.encrypt(4, "test.txt"), "alex")
+        self.assertEqual(Cipher.encrypt(30, "test.txt"), "alex")
 
     def test_encrypt_not_equal(self):
-        self.assertNotEqual(main.encrypt("e", 7, "test.txt"), "alex")
-        self.assertNotEqual(main.encrypt("e", 3, "test.txt"), "alex")
+        self.assertNotEqual(Cipher.encrypt(7, "test.txt"), "alex")
+        self.assertNotEqual(Cipher.encrypt(3, "test.txt"), "alex")
 
     def test_decrypt_equal(self):
-        self.assertEqual(main.decrypt("test.txt", 4), "what")
-        self.assertEqual(main.decrypt("test.txt", 30), "what")
-        self.assertEqual(main.decrypt("test.txt"), "what")
+        self.assertEqual(Cipher.decrypt("test.txt", 4), "what")
+        self.assertEqual(Cipher.decrypt("test.txt", 30), "what")
+        self.assertEqual(Cipher.decrypt("test.txt"), "what")
 
     def test_decrypt_not_equal(self):
-        self.assertNotEqual(main.decrypt("test.txt", 7), "what")
-        self.assertNotEqual(main.decrypt("test.txt", 3), "what")
-        self.assertNotEqual(main.decrypt("test.txt"), "tahw")
+        self.assertNotEqual(Cipher.decrypt("test.txt", 7), "what")
+        self.assertNotEqual(Cipher.decrypt("test.txt", 3), "what")
+        self.assertNotEqual(Cipher.decrypt("test.txt"), "tahw")
 
 
 if __name__ == "__main__":
